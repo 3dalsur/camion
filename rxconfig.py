4f498291183ba84520501988f519efffcff50f7e
3dalsur <softwarepes@gmail.com>
2026-04-20 11:50:46 -0400

    Add files via upload

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,14 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="camion_app"
+    app_name="camion_app",
+
+    # ?? CLAVE PARA RENDER
+    backend_host="0.0.0.0",
+    backend_port=10000,
+
+    # Evita auto-plugins innecesarios
+    disable_plugins=[
+        "reflex.plugins.sitemap.SitemapPlugin"
+    ]
 )
